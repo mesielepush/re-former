6005d53927ea8fc14712b482cc5fd17dc268a7d7
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-12-12 12:53:12 -0600

    redoing for some weird problem with new user creation

--- reformer/app/controllers/users_controller.rb
+++ reformer/app/controllers/users_controller.rb
@@ -25,16 +25,15 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    puts @user.valid?
+    if @user.save
+      redirect_to @user
+      
+    else
+      puts 'NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO'
+      render :new
     end
+    
   end
 
   # PATCH/PUT /users/1
--- reformer/config/routes.rb
+++ reformer/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'users/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create, :edit, :update]
+  resources :users, only: [:new, :create, :edit, :update, :show]
 end
